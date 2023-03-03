c1e81dacc5270d0c1effdc52672ce8333fa10fe9
juseong <wjdwntjd55@naver.com>
2023-03-03 17:14:11 +0900

    쇼핑앱 UI 구성 : 화면이동 설계 2. Fragment 추가 및 화면 연결

---
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.shoppi.app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class HomeFragment:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+        button.setOnClickListener {
+
+            // FragmentManager : Fragment 를 추가, 삭제, 교체를 담당한다
+            // 종류 : parentFragmentManager, childFragmentManager
+            // 여기서 HomeFragment 는 host 의 역할을 하므로 parentFragmentManager 를 참조해야한다
+            // Transaction 이라함은 Fragment 를 추가, 삭제, 교체를 요청하는 걸 의미
+            //  - beginTransaction() : Transaction 객체를 반환
+            //  - commit() : Transaction 이 실행하기 위해서는 반드시 선언해줘야한다
+
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.add(R.id.container_main, ProductDetailFragment())
+            transaction.commit()
+
+        }
+    }
+
+}
